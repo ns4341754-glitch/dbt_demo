4c36f04b0ad0c2043354bef5cf10f8b194b8a504
ns4341754@gmail.com <ns4341754@gmail.com>
2026-01-07 19:09:33 +0000

    format

--- macros/date_funtion.sql
+++ macros/date_funtion.sql
@@ -1,16 +1,19 @@
 {% macro season_of_year(timestamp_col) %}
-  CASE
-    WHEN MONTH(TO_TIMESTAMP({{ timestamp_col }})) IN (12, 1, 2) THEN 'winter'
-    WHEN MONTH(TO_TIMESTAMP({{ timestamp_col }})) IN (3, 4, 5)  THEN 'spring'
-    WHEN MONTH(TO_TIMESTAMP({{ timestamp_col }})) IN (6, 7, 8)  THEN 'summer'
-    ELSE 'autumn'
-  END 
+    case
+        when month(to_timestamp({{ timestamp_col }})) in (12, 1, 2)
+        then 'winter'
+        when month(to_timestamp({{ timestamp_col }})) in (3, 4, 5)
+        then 'spring'
+        when month(to_timestamp({{ timestamp_col }})) in (6, 7, 8)
+        then 'summer'
+        else 'autumn'
+    end
 {% endmacro %}
 
-{% macro day (x) %}
- case
-  when dayname(TO_TIMESTAMP({{x}})) in ('Sat' , 'Sun')
-  then 'weekend'
-  else 'businessday'
-  end 
-  {% endmacro %}
+{% macro day(x) %}
+    case
+        when dayname(to_timestamp({{ x }})) in ('Sat', 'Sun')
+        then 'weekend'
+        else 'businessday'
+    end
+{% endmacro %}
